9ed069fd05d48b653da7a1dd068604c514f540c5
symberke <symberke@gmail.com>
2016-01-06 14:43:40 -0500

    Updated ouoio.js

--- ouoio.js
+++ ouoio.js
@@ -1,12 +1,25 @@
 // ==UserScript==
-// @name         ouo.io skip
+// @name         ouo.io skip attempt
 // @namespace    http://your.homepage/
 // @version      0.1
 // @description  enter something useful
 // @author       You
 // @match        http://ouo.io/*
 // @grant        none
-// ==/UserScript==*/
+// @downloadurl  https://raw.githubusercontent.com/symberke/tmscripts/master/ouoio.js
+// ==/UserScript==
+
+/*
+btn = document.getElementsByClassName("btn")
+
+// random number from 250 to 750
+delay = Math.round(500 * Math.random() + 250);
+
+if (btn.length > 0) {
+    setTimeout( function(){ btn[0].click() }, delay);
+}
+
+*/
 
 (function() {
     btn = document.getElementsByClassName("btn");
